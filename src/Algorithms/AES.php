3345904ae14be5e236d2586d287b98cff96f5dc0
hopher <565983236@qq.com>
2019-02-28 15:57:23 +0800

    add AES && RSA

---
+++ src/Algorithms/AES.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * 对称加密算法
+ */
+
+class RSA
+{
+    public static function encode()
+    {
+
+    }
+
+    public static function decode()
+    {
+
+    }    
+}
---
+++ src/Algorithms/RSA.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * 非对称加密算法
+ */
+
+class RSA
+{
+    public static function encode()
+    {
+
+    }
+
+    public static function decode()
+    {
+
+    }
+}
